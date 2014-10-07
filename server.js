efc7c646f952f9e35655de386c8366cb3378ae0c
Aiden Montgomery <aiden@constructivecoding.com>
2014-10-07 11:53:54 +0100

    Added mongoose and Job API

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job');
+
 var app = express();
 
 app.set('views', __dirname);
@@ -6,8 +9,24 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api-jobs', function(req, res) {
+  mongoose.model('Job').find({}).exec(function(error, collection) {
+    res.send(collection);
+  });
+});
+
 app.get('*', function(req, res) {
   res.render('index');
+});
+
+// mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://jobfinder:medusa@ds043210.mongolab.com:43210/heroku_app30484412');
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+  console.log('Connected to mongodb succesfully');
+  jobModel.seedJobs();
 });
 
 app.listen(process.env.PORT || 3000);
